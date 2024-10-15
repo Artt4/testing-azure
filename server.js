1a5aa86be40fb62b9b99befde730589391a4bc8c
Aortta <44739519+Artt4@users.noreply.github.com>
2024-10-15 21:42:59 +0300

    test

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to My Node.js App!</h1>');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
